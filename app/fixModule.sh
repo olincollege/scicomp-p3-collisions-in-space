720f692b8afcef77b9760c0a6d6549276d69d8f5
Jasper Katzban <jasperkatzban@gmail.com>
2023-04-04 23:22:57 -0400

    fix fixModule on macOS

--- app/fixModule.sh
+++ app/fixModule.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 path='node_modules/canvas-sketch-cli/src/index.js'
-sed -i 's/nodeModules: false/nodeModules: true/' $path
+sed -i '' 's/nodeModules: false/nodeModules: true/' $path
+
+echo "Success! canvas-sketch-cli module changed to support imports."
